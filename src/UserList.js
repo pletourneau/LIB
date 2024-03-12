5aedf14c3675dde75fab0c61623c1974d41c8e1b
Paul LeTourneau <pletourneau62@gmail.com>
2024-03-12 13:51:37 -0700

    add userlist component

---
+++ src/UserList.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/users")
+      .then((response) => setUsers(response.data))
+      .catch((error) =>
+        console.error("There was an error retrieving the user data:", error)
+      );
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>
+            {user.name} - Coins: {user.coins}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
